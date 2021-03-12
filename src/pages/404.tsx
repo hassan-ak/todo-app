7114c5142948997ec6b47fe08a73212960038919
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-13 03:56:17 +0500

    Step02:- Add Site title and global styling
    1:- Installed
    	a:- "yarn add gatsby-plugin-react-helmet react-helmet" to add site title
    	b:- "yarn add @types/react-helmet" types defination for helmet
    2:- Updated
    	a:- src/pages/--- to add title to the site
    		i:- index.tsx
    		ii:- 404.tsx
    	b:- gatsby-config.js to add helmet plugin to the site
    3:- Created
    	a:- src/styles/global.css for defining styles to be used at global level
    	b:- gatsby-browser.js to add styling to app

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 export default function Error() {
-  return <div>Error Page</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Todo's | 404</title>
+      </Helmet>
+      <div>Error Page</div>
+    </div>
+  );
 }
