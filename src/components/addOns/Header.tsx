adc30e9cf079b29dd58f8e230fd7de9ca4aecd8f
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-13 04:21:21 +0500

    Step05 - Use Header and Footer in the App
    1:- Created
    	a:- src/components/addOns/Layout.tsx to define a wraper for so that Header and Footer can be used in the App
    2:- Updated
    	b:- src/pages/--- to wrap content of respective pages in the app
    		i:- index.tsx
    		ii:- 404.tsx

--- src/components/addOns/Header.tsx
+++ src/components/addOns/Header.tsx
@@ -4,13 +4,20 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 import "./header.css";
+import { navigate } from "gatsby";
 
 const Header = () => {
   return (
     <div className='root'>
       <AppBar position='static' elevation={1}>
         <Toolbar className='transparent'>
-          <IconButton edge='start' aria-label='Todo App'>
+          <IconButton
+            edge='start'
+            aria-label='Todo App'
+            onClick={() => {
+              navigate("/");
+            }}
+          >
             <AssignmentIcon />
           </IconButton>
           <Typography variant='h5'>TaskBox</Typography>
