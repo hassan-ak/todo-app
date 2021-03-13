b7ccbcd2d0d3054129f08225e1b359320348b090
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-14 02:42:34 +0500

    Step10- Update Landing page based on user
    1:- Updated
    	a:- src/components/landing/
    		i:- Landing.tsx to change page display based on user status
    		ii:- landing.css to improve styling for landing page

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,16 +1,58 @@
 import { Button } from "@material-ui/core";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import netlifyIdentity from "netlify-identity-widget";
 import "./landing.css";
+import { navigate } from "gatsby";
 
 export const Landing = () => {
+  const [user, setUser] = useState("");
   useEffect(() => {
     netlifyIdentity.init({});
   });
+  netlifyIdentity.on("login", (user) => {
+    netlifyIdentity.close();
+    setUser(user.user_metadata.full_name);
+  });
+  netlifyIdentity.on("logout", () => {
+    setUser("");
+  });
+  console.log();
+  if (user === "") {
+    return (
+      <div className='landingContainer'>
+        <div className='landingContent'>
+          <h1>Manage your Todo's</h1>
+        </div>
+        <div className='buttonDiv'>
+          <Button
+            className='landingButton'
+            onClick={() => {
+              netlifyIdentity.open();
+            }}
+          >
+            Login
+          </Button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className='landingContainer'>
       <div className='landingContent'>
-        <h1>Manage your Todo's</h1>
+        <h1>Welcome {user}!</h1>
+      </div>
+      <div className='landingContent'>
+        <h3>Manage your Todo's</h3>
+      </div>
+      <div className='buttonDiv'>
+        <Button
+          className='landingButton'
+          onClick={() => {
+            navigate("/app");
+          }}
+        >
+          DashBoard
+        </Button>
       </div>
       <div className='buttonDiv'>
         <Button
@@ -19,7 +61,7 @@ export const Landing = () => {
             netlifyIdentity.open();
           }}
         >
-          Login
+          LogOut
         </Button>
       </div>
     </div>
